ac6c7093eca1a9ab7217a502066ae04fd6deaee7
Chris Little <chrisclittle@gmail.com>
2015-05-07 14:41:09 -0700

    added build/test script & simple pylint.rc

---
+++ btest.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+sudo rm -rf ./usfm2osis.egg-info
+sudo rm -rf ./dist
+
+sudo rm -rf ./build
+python setup.py build
+sudo python setup.py install
+
+sudo rm -rf ./build
+python3 setup.py build
+sudo python3 setup.py install
+
+nosetests -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=usfm2osis .
+nosetests3 -v --with-coverage --cover-erase --cover-html --cover-branches --cover-package=usfm2osis .
+
+pylint --rcfile=pylint.rc usfm2osis > pylint.log
+
+sudo python3 setup.py sdist
+sudo python3 setup.py bdist_wheel
