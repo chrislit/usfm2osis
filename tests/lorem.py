ff4873329921133052046f1f51820ccf61909148
Christopher C. Little <chrisclittle@gmail.com>
2015-05-12 21:32:44 -0700

    PEP8 fixes

--- tests/lorem.py
+++ tests/lorem.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re, os, codecs
+import re
+import os
+import codecs
 
 loremText = open('lorem.txt').read().strip()
 
-#lorem = list(set(re.sub(r'[^a-zA-Z ]', r' ', loremText).lower().split()))
-#utext = u'\\v adhkljfsaa Мыа но льабятюр дэлььикат 1234άτεψε τις λέξεις 5473'
+# lorem = list(set(re.sub(r'[^a-zA-Z ]', r' ', loremText).lower().split()))
+# utext = u'\\v adhkljfsaa Мыа но льабятюр дэлььикат 1234άτεψε τις λέξεις 5473'
 lorem = loremText.split()
+
 
 def loremize(w):
     global i
@@ -25,11 +28,10 @@ for filename in sorted(os.listdir('usfmSamples_orig')):
     infile = codecs.open('usfmSamples_orig/'+filename, 'r', 'utf-8')
     outfile = codecs.open('usfmSamples/'+filename, 'w', 'utf-8')
     i = 0
-    
+
     print filename
     for l in infile:
         if re.search(r'(\ide?|\periph)\b', l):
             outfile.write(l)
         else:
             outfile.write(' '.join(map(loremize, l.split()))+'\n')
-    
